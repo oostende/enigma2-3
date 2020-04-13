edc2182f122ecae1b5c3f1825718d09a508c7844
oostende <redouaneelrhachi@hotmail.com>
2020-04-13 12:02:34 +0200

    Update skin.py

--- skin.py
+++ skin.py
@@ -16,7 +16,7 @@ from Tools.Directories import SCOPE_CONFIG, SCOPE_CURRENT_LCDSKIN, SCOPE_CURRENT
 from Tools.Import import my_import
 from Tools.LoadPixmap import LoadPixmap
 
-DEFAULT_SKIN = "GigabluePax/skin.xml"
+DEFAULT_SKIN = "oDreamy-FHD/skin.xml"
 # DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD/skin.xml" or "PLi-HD/skin.xml"  # SD hardware is no longer supported by the default skin.
 EMERGENCY_SKIN = "skin_default/skin.xml"
 DEFAULT_DISPLAY_SKIN = "skin_display.xml"
